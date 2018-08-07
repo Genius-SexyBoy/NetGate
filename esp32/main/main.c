dbedf73c4d977d04cf4bff025244c2bc64fea59f
Genius-SexyBoy <879255943@qq.com>
2018-08-07 12:46:25 +0000

    add TCP server and uart driver

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -1,4 +1,5 @@
 #include "eth.h"
+#include "UART.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
 
@@ -9,13 +10,11 @@
 #define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
 #define EXAMPLE_MAX_STA_CONN       CONFIG_MAX_STA_CONN
 
+#define BUF_SIZE (1024)
 
+TaskHandle_t th[12];
 
-
-static TaskHandle_t ETH_Task_Handle;
-
-
-
+static void mongoose_task(void *pvParameter);
 
 static const char *TAG = "main";
 
@@ -50,9 +49,11 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
         break;
       case SYSTEM_EVENT_ETH_GOT_IP:
         ESP_LOGI(TAG,"ETH_GOT_IP");
+        xTaskCreatePinnedToCore(mongoose_task, "mongoose_task", 4096, NULL, 2, &th[0], 1);  //creat tcp task
         break;
       case SYSTEM_EVENT_ETH_DISCONNECTED:
         ESP_LOGI(TAG,"ETH Disconnected");
+        vTaskDelete(th[0]);               //delete the task
         break;
       case SYSTEM_EVENT_ETH_STOP:
         ESP_LOGI(TAG,"ETH Stopped");
@@ -75,7 +76,6 @@ void wifi_init_sta()
             .password = EXAMPLE_ESP_WIFI_PASS
         },
     };
-
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
     ESP_ERROR_CHECK(esp_wifi_start() );
@@ -94,41 +94,129 @@ static void ETH_Task(void *pvParameter)
   {
     vTaskDelay(1);
   }
-  
 }
 
-static void ev_handler(struct mg_connection *c, int ev, void *p) {
-  if (ev == MG_EV_HTTP_REQUEST) {
-    struct http_message *hm = (struct http_message *) p;
+static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
+  struct mbuf *io = &nc->recv_mbuf;
 
-    // We have received an HTTP request. Parsed request is contained in `hm`.
-    // Send HTTP reply to the client which shows full original request.
-    mg_send_head(c, 200, hm->message.len, "Content-Type: text/plain");
-    mg_printf(c, "%.*s", (int)hm->message.len, hm->message.p);
+  switch (ev) 
+  {
+    case MG_EV_RECV:
+      // This event handler implements simple TCP echo server
+//      mg_send(nc, io->buf, io->len);  // Echo received data back
+         ESP_LOGI(TAG,"%s",io->buf);
+         mbuf_remove(io, io->len);      // Discard data from recv buffer
+         break;
+    default:break;
   }
 }
 
 static void mongoose_task(void *pvParameter)
 {
-  static const char *s_http_port = "8000";
-  struct mg_mgr mgr;
-  struct mg_connection *c;
+   struct mg_mgr mgr;
   mg_mgr_init(&mgr, NULL);
-  c = mg_bind(&mgr, s_http_port, ev_handler);
-  mg_set_protocol_http_websocket(c);
-
-  while(1)
-  {
+  mg_bind(&mgr, "1234", ev_handler);
+  while(1) 
+  {  
     mg_mgr_poll(&mgr, 1000);
     vTaskDelay(1);
+  }  
+}
+
+static void uart_event_task(void *pvParameters)
+{
+    uart_event_t event;
+    size_t buffered_size;
+    uint8_t* dtmp = (uint8_t*) malloc(RD_BUF_SIZE);
+    for(;;) {UART_NUM_1
+        //Waiting for UART event.
+        if(xQueueReceive(uart0_queue, (void * )&event, (portTickType)portMAX_DELAY)) {
+            bzero(dtmp, RD_BUF_SIZE);
+            ESP_LOGI(TAG, "uart[%d] event:", EX_UART_NUM);
+            switch(event.type) {
+                //Event of UART receving data
+                /*We'd better handler data event fast, there would be much more data events than
+                other types of events. If we take too much time on data event, the queue might
+                be full.*/
+                case UART_DATA:
+                    ESP_LOGI(TAG, "[UART DATA]: %d", event.size);
+                    uart_read_bytes(EX_UART_NUM, dtmp, event.size, portMAX_DELAY);
+                    ESP_LOGI(TAG, "[DATA EVT]:");
+                    uart_write_bytes(EX_UART_NUM, (const char*) dtmp, event.size);
+                    break;
+                //Event of HW FIFO overflow detected
+                case UART_FIFO_OVF:
+                    ESP_LOGI(TAG, "hw fifo overflow");
+                    // If fifo overflow happened, you should consider adding flow control for your application.
+                    // The ISR has already reset the rx FIFO,
+                    // As an example, we directly flush the rx buffer here in order to read more data.
+                    uart_flush_input(EX_UART_NUM);
+                    xQueueReset(uart0_queue);
+                    break;
+                //Event of UART ring buffer full
+                case UART_BUFFER_FULL:
+                    ESP_LOGI(TAG, "ring buffer full");
+                    // If buffer full happened, you should consider encreasing your buffer size
+                    // As an example, we directly flush the rx buffer here in order to read more data.
+                    uart_flush_input(EX_UART_NUM);
+                    xQueueReset(uart0_queue);
+                    break;
+                //Event of UART RX break detected
+                case UART_BREAK:
+                    ESP_LOGI(TAG, "uart rx break");
+                    break;
+                //Event of UART parity check error
+                case UART_PARITY_ERR:
+                    ESP_LOGI(TAG, "uart parity error");
+                    break;
+                //Event of UART frame error
+                case UART_FRAME_ERR:
+                    ESP_LOGI(TAG, "uart frame error");
+                    break;
+                //UART_PATTERN_DET
+                case UART_PATTERN_DET:
+                    uart_get_buffered_data_len(EX_UART_NUM, &buffered_size);
+                    int pos = uart_pattern_pop_pos(EX_UART_NUM);
+                    ESP_LOGI(TAG, "[UART PATTERN DETECTED] pos: %d, buffered size: %d", pos, buffered_size);
+                    if (pos == -1) {
+                        // There used to be a UART_PATTERN_DET event, but the pattern position queue is full so that it can not
+                        // record the position. We should set a larger queue size.
+                        // As an example, we directly flush the rx buffer here.
+                        uart_flush_input(EX_UART_NUM);
+                    } else {
+                        uart_read_bytes(EX_UART_NUM, dtmp, pos, 100 / portTICK_PERIOD_MS);
+                        uint8_t pat[PATTERN_CHR_NUM + 1];
+                        memset(pat, 0, sizeof(pat));
+                        uart_read_bytes(EX_UART_NUM, pat, PATTERN_CHR_NUM, 100 / portTICK_PERIOD_MS);
+                        ESP_LOGI(TAG, "read data: %s", dtmp);
+                        ESP_LOGI(TAG, "read pat : %s", pat);
+                    }
+                   mongoose break;
+                //Others
+                default:
+                    ESP_LOGI(TAG, "uart event type: %d", event.type);
+                    break;
+            }
+        }
+    }
+    free(dtmp);
+    dtmp = NULL;
+    vTaskDelete(NULL);
+}
+
+static void UART_task(void *pvParameter)
+{
+  UART_Init();
+  xTaskCreatePinnedToCore(uart_event_task, "uart_event_task", 2048, NULL, 2, NULL,1);
+  while(1)
+  {
+    vTaskDelay(1);
   }
-  
-  
 }
 
 
 void app_main() 
 {
- xTaskCreatePinnedToCore(ETH_Task, "ETH_Task", 2048, NULL, 2, NULL,0);
- xTaskCreatePinnedToCore(mongoose_task, "mongoose_task", 4096, NULL, 2, NULL,1);
+ xTaskCreatePinnedToCore(ETH_Task, "ETH_Task", 2048, NULL, 2, NULL, 0);
+ xTaskCreatePinnedToCore(UART_task, "UART_task", 1024, NULL, 1, NULL, 0);
 }
