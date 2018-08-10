06a57a71687842fa52572fd8438a26bc3629f5f8
Genius-SexyBoy <879255943@qq.com>
2018-08-10 14:32:26 +0800

    update uart function

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -12,14 +12,10 @@
 
 #include "mongoose.h"
 
-#define EXAMPLE_ESP_WIFI_MODE_AP   CONFIG_ESP_WIFI_MODE_AP //TRUE:AP FALSE:STA
-#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
-#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
-#define EXAMPLE_MAX_STA_CONN       CONFIG_MAX_STA_CONN
 
 #define BUF_SIZE (1024)
 
-TaskHandle_t th[12];
+TaskHandle_t Mon_Handle;
 
 static void mongoose_task(void *pvParameter);
 
@@ -56,11 +52,11 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
         break;
       case SYSTEM_EVENT_ETH_GOT_IP:
         ESP_LOGI(TAG,"ETH_GOT_IP");
-        //xTaskCreatePinnedToCore(mongoose_task, "mongoose_task", 4096, NULL, 2, &th[0], 1);  //creat tcp task
+        xTaskCreatePinnedToCore(mongoose_task, "mongoose_task", 4096, NULL, 2, &Mon_Handle, 1);  //creat tcp task
         break;
       case SYSTEM_EVENT_ETH_DISCONNECTED:
         ESP_LOGI(TAG,"ETH Disconnected");
-        //vTaskDelete(th[0]);               //delete the task
+        vTaskDelete(Mon_Handle);               //delete the task
         break;
       case SYSTEM_EVENT_ETH_STOP:
         ESP_LOGI(TAG,"ETH Stopped");
@@ -71,30 +67,10 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
     return ESP_OK;
 }
 
-void wifi_init_sta()
+
+static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) 
 {
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL) );
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .password = EXAMPLE_ESP_WIFI_PASS
-        },
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
-
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
-    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
-             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
-}
-
-static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
   struct mbuf *io = &nc->recv_mbuf;
-
   switch (ev) 
   {
     case MG_EV_RECV:
@@ -109,7 +85,7 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
 
 static void mongoose_task(void *pvParameter)
 {
-   struct mg_mgr mgr;
+  struct mg_mgr mgr;
   mg_mgr_init(&mgr, NULL);
   mg_bind(&mgr, "1234", ev_handler);
   while(1) 
@@ -121,20 +97,15 @@ static void mongoose_task(void *pvParameter)
 
 void uart_ondata(uint8_t *data, uint16_t len)
 {
-  uint8_t *pa = NULL;
-  uint8_t *pb = NULL;
-  uint8_t patter[20] = {0x0A, 0x0B, 0x0C, 0};
-
-  pa = (uint8_t *)strstr((const char *)data,(const char *)patter);
-
-  if(pa) 
+  if((data[0] == 0xFE) && (data[1] == 0x64) && (data[2] == 0xFF))
   {
-    vTaskDelay(500 / portTICK_RATE_MS);
-    printf("---------------------test-----------------\n");
-  } 
-  else 
+    printf("Shakehands completed!\n");
+    printf("Receive %d data\n",len);
+  }
+  else
   {
-    printf("SB\n");
+    printf("Send to Server!\n");
+    printf("Receive %d data\n",len);
   }
 }
 
@@ -147,5 +118,4 @@ void app_main()
   ESP_LOGI(TAG, "NetGate\n");
   tcpip_adapter_init();
   eth_install(event_handler, NULL);
-  
 }
