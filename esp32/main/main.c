f72fae903e27ef645288312d321f9dca63d3a3e6
Genius-SexyBoy <879255943@qq.com>
2018-09-13 15:37:37 +0000

    add local ip setting funtion

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -26,8 +26,8 @@
 #include "lwip/dns.h"
 
 
-nvs_handle my_handle;
-
+nvs_handle nvs_remote_handle;
+nvs_handle nvs_local_handle;
 
 extern EventGroupHandle_t eth_event_group;
 
@@ -41,6 +41,7 @@ typedef struct {
   uint8_t data[512];
   int32_t len;
 } data_buf_t;
+
 
 
 static const char *TAG = "main";
@@ -84,24 +85,24 @@ static void tcp_client_task(void *pvParameter)
   *  and port from the nvs flash
   */
   size_t size;
-  esp_err_t err = nvs_open("Remote", NVS_READWRITE, &my_handle);
+  esp_err_t err = nvs_open("Remote", NVS_READWRITE, &nvs_remote_handle);
   if (err != ESP_OK) 
     ESP_LOGW(TAG, "TCP:flash open error!\n");
-  nvs_get_str(my_handle, "IP", NULL, &size);
+  nvs_get_str(nvs_remote_handle, "IP", NULL, &size);
   char ip_addr[size];
-  nvs_get_str(my_handle, "IP", ip_addr, &size);
+  nvs_get_str(nvs_remote_handle, "IP", ip_addr, &size);
 
-  nvs_get_str(my_handle, "Port", NULL, &size);
-  char port[10];
-  nvs_get_str(my_handle, "Port", port, &size);
+  nvs_get_str(nvs_remote_handle, "Port", NULL, &size);
+  char port[size];
+  nvs_get_str(nvs_remote_handle, "Port", port, &size);
   int ip_port = atoi(port);
-  nvs_close(my_handle);
+  nvs_close(nvs_remote_handle);
 
   
-  printf("%d\n", ip_port);
+  printf("TCP server %s:%d\n", ip_addr,ip_port);
   while(1)
   {
-    vTaskDelay(1000 / portTICK_RATE_MS);
+    //vTaskDelay(1000 / portTICK_RATE_MS);
     int  sockfd = 0, iResult = 0;
 
     struct  sockaddr_in serv_addr;
@@ -124,8 +125,6 @@ static void tcp_client_task(void *pvParameter)
 
     if (conn_ret == -1)
     {
-      //ESP_LOGE(TAG, "%s", serv_addr.sin_addr.s_addr);
-      ESP_LOGE(TAG, "%d", serv_addr.sin_port);
       ESP_LOGE(TAG, "connect to server failed! errno=%d", errno);
       close(sockfd);
       continue;
@@ -276,11 +275,23 @@ void at_commend_task(void *pvParameter)
           if((at_commend_struct.data[5] == 'I') && (at_commend_struct.data[6] == 'P') && (at_commend_struct.data[7] == '='))
           {
             //AT+LCIP=
-            
+            uart_write_bytes(UART_NUM_0, (ret + 1), strlen(ret));
+            my_nvs_write(nvs_local_handle, "Local", "IP", (ret + 1));
+            ESP_LOGI(TAG, "Set local IP success!\n");
           }
           else if((at_commend_struct.data[5] == 'G') && (at_commend_struct.data[6] == 'W') && (at_commend_struct.data[7] == '='))
           {
+            //AT+LCGW=
+            uart_write_bytes(UART_NUM_0, (ret + 1), strlen(ret));
+            my_nvs_write(nvs_local_handle, "Local", "GW", (ret + 1));
             ESP_LOGI(TAG, "Set local gateway success!\n");
+          }
+          else if((at_commend_struct.data[5] == 'N') && (at_commend_struct.data[6] == 'M') && (at_commend_struct.data[7] == '='))
+          {
+            //AT+LCNM=
+            uart_write_bytes(UART_NUM_0, (ret + 1), strlen(ret));
+            my_nvs_write(nvs_local_handle, "Local", "NM", (ret + 1));
+            ESP_LOGI(TAG, "Set local netmask success!\n");
           }
         }
         else if((at_commend_struct.data[3] == 'R') && (at_commend_struct.data[4] == 'M'))
@@ -292,17 +303,18 @@ void at_commend_task(void *pvParameter)
             if(ret != NULL)
             {
               uart_write_bytes(UART_NUM_0, (ret + 1), strlen(ret));
-              my_nvs_write(my_handle, "Remote", "IP", (ret + 1));
+              my_nvs_write(nvs_remote_handle, "Remote", "IP", (ret + 1));
               ESP_LOGI(TAG, "Set remote IP success!\n");            
             }
           }
           else if((at_commend_struct.data[5] == 'P') && (at_commend_struct.data[6] == 'O') && (at_commend_struct.data[7] == '='))
           {
+            //AT+RMPO=
             ret = strchr((const char *)at_commend_struct.data, ch);
             if(ret != NULL)
             {
               uart_write_bytes(UART_NUM_0, (ret + 1), strlen(ret));
-              my_nvs_write(my_handle, "Remote", "Port", (ret + 1));
+              my_nvs_write(nvs_remote_handle, "Remote", "Port", (ret + 1));
               ESP_LOGI(TAG, "Set remote port success!\n");            
             }    
           }
@@ -324,14 +336,14 @@ void at_commend_task(void *pvParameter)
 // void flash_read_task(void *pvParameter)
 // {
 //   size_t size;
-// //  my_nvs_write(my_handle, MY_NAMESPACE, "key_one", "kangkang");
-//   esp_err_t err = nvs_open(MY_NAMESPACE, NVS_READWRITE, &my_handle);
+// //  my_nvs_write(nvs_remote_handle, MY_NAMESPACE, "key_one", "kangkang");
+//   esp_err_t err = nvs_open(MY_NAMESPACE, NVS_READWRITE, &nvs_remote_handle);
 //   if (err != ESP_OK) 
 //     ESP_LOGW(TAG, "flash open error!\n");
-//   nvs_get_str(my_handle, "AT", NULL, &size);
+//   nvs_get_str(nvs_remote_handle, "AT", NULL, &size);
 //   char* ip_addr = malloc(size);
-//   nvs_get_str(my_handle, "AT", ip_addr, &size);
-//   nvs_close(my_handle);
+//   nvs_get_str(nvs_remote_handle, "AT", ip_addr, &size);
+//   nvs_close(nvs_remote_handle);
 //   while(1)
 //   {
 // //    uart_write_bytes(UART_NUM_0, (const char *)ip_addr, size);
@@ -345,10 +357,13 @@ void at_commend_task(void *pvParameter)
 
 void app_main() 
 {
+  size_t size;
   esp_err_t err = nvs_flash_init();
+
   eth_event_group = xEventGroupCreate();
   uart_queue_handle = xQueueCreate(100, sizeof(data_buf_t));
   tcp_queue_handle = xQueueCreate(100, sizeof(data_buf_t));
+
   if (err == ESP_ERR_NVS_NO_FREE_PAGES)
   {
     ESP_ERROR_CHECK(nvs_flash_erase());
@@ -356,13 +371,31 @@ void app_main()
   }
   ESP_ERROR_CHECK(err); 
   ets_delay_us(100000);
+
+/*
+*   read the local ip address, gateway ,netmask from flash
+*/
+  err = nvs_open("Local", NVS_READWRITE, &nvs_local_handle);
+  if (err != ESP_OK) 
+    ESP_LOGW(TAG, "app_main:nvs open error!\n");
+  nvs_get_str(nvs_local_handle, "IP", NULL, &size);
+  char local_ip[size];
+  nvs_get_str(nvs_local_handle, "IP", local_ip, &size);
+
+  nvs_get_str(nvs_local_handle, "GW", NULL, &size);
+  char local_gw[size];
+  nvs_get_str(nvs_local_handle, "GW", local_gw, &size);
+
+  nvs_get_str(nvs_local_handle, "NM", NULL, &size);
+  char local_nm[size];
+  nvs_get_str(nvs_local_handle, "NM", local_nm, &size);
+  nvs_close(nvs_local_handle);
+
   uart_init();
   eth_install(event_handler, NULL);
   eth_config("ESP32-GateWay", inet_addr("192.168.3.66"),
-                              inet_addr("192.168.3.1"), inet_addr("255.255.255.0"), inet_addr("192.168.3.1"), 0);
-  xTaskCreatePinnedToCore(&uart_task, "uart_task", 4 * 1024, NULL, 6, NULL, 1);
+                              inet_addr("192.168.3.1"), inet_addr("255.255.255.0"), 0, 0);
+  xTaskCreate(&uart_task, "uart_task", 4 * 1024, NULL, 6, NULL);
   xTaskCreatePinnedToCore(&at_commend_task, "at_commend_task", 4 * 1024, NULL, 4, NULL, 1);
   xTaskCreate(&tcp_client_task, "tcp_client_task", 8 * 1024, NULL, 8, NULL);
-//  xTaskCreate(&flash_read_task, "flash_task", 4096, NULL, 2, NULL);
- // xTaskCreate(&flash_write_task, "flash_task", 4096, NULL, 1, NULL);
 }
